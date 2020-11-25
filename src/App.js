6308de4bae22cbc66e618d8292a5c71cd3d300c2
Pawel Bozejewski <bozejewski@gmail.com>
2020-11-25 21:03:44 +0100

    setState

--- src/App.js
+++ src/App.js
@@ -1,27 +1,36 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import SeasonDisplay from './SeasonsDisplay';
 
-
 class App extends Component {
-  constructor( props ) {
+  constructor(props) {
     super(props);
 
     this.state = {
-      lat:"null"
+      lat: null,
+      errorMessage: '',
     };
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({ lat: position.coords.latitude });
+      },
+      (err) => {
+        this.setState({ errorMessage: err.message });
+      }
+    );
   }
 
-  render() { 
-    window.navigator.geolocation.getCurrentPosition(
-      position => console.log(position),
-      err => console.log(err)
-    );
-  return (
-    <div className="App">
-    
-    <SeasonDisplay/>
-    </div>
-  );
-}}
+  render() {
+    const { errorMessage, lat } = this.state;
+
+    if (errorMessage && !lat) {
+      return <div>{this.state.errorMessage}</div>;
+    }
+    if (lat && !errorMessage) {
+      return <div className='App'> Latitude: {lat} </div>;
+    }
+    return <div>Loading...</div>;
+  }
+}
 
 export default App;
