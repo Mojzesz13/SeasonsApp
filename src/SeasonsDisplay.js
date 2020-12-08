c0a276a8fff42b54267bfc8869c868d7bf3f4128
Pawel Bozejewski <bozejewski@gmail.com>
2020-12-08 21:50:36 +0100

    season

--- src/SeasonsDisplay.js
+++ src/SeasonsDisplay.js
@@ -1,11 +1,40 @@
 import React, { Component } from 'react';
+import 'semantic-ui-css/semantic.min.css';
+
+const seasonConfig = {
+  summer: {
+    text: "Let's hit the beach!",
+    iconName: 'sun',
+  },
+  winter: {
+    text: 'Burr, it is chilly',
+    iconName: 'snowflake',
+  },
+};
+
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? 'summer' : ' winter';
+  } else {
+    return lat > 0 ? 'winter' : 'summer';
+  }
+};
 
 class SeasonDisplay extends Component {
   constructor(props) {
     super(props);
   }
   render() {
-    return <div>Latitude: {this.props.lat}</div>;
+    const season = getSeason(this.props.lat, new Date().getMonth());
+    const { text, iconName } = seasonConfig[season];
+    
+    return (
+      <div>
+        <i className={`${iconName} icon`} />
+        <h1>{text}</h1>
+        <i className={`${iconName} icon`} />
+      </div>
+    );
   }
 }
 export default SeasonDisplay;
